d58047c7a533cf742535ca9afe5b317cd65ce160
Caio Campos <caioalvescampos2010@gmail.com>
2025-10-03 15:42:22 -0400

    Commit limpo

--- frontend/cryptoquest/lib/core/config/app_config.dart
+++ frontend/cryptoquest/lib/core/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  // static const String baseUrl = "https://cryptoquest-backend.onrender.com"; 
-  static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "https://cryptoquest-backend.onrender.com"; 
+  // static const String baseUrl = "http://127.0.0.1:8000";
 }
